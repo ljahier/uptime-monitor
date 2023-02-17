7a1682f7fa28c2e786d22e4f067430f8235051ba
Lucas Jahier <lucas.jahier@jahier.dev>
2023-02-17 08:13:57 +0100

    feat: add goroutine to run all request at the same time

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"sync"
 	"time"
 
 	probing "github.com/prometheus-community/pro-bing"
@@ -25,6 +26,7 @@ type Monitor struct {
 	StatusCode   int
 	ResponseTime time.Duration
 	Config       MonitorConfig
+	HasChecked   bool
 }
 
 func (m *Monitor) checkICMP() {
@@ -39,48 +41,61 @@ func (m *Monitor) checkICMP() {
 	if err != nil {
 		log.Default().Println(err)
 	}
-	m.Status, m.ResponseTime = true, pinger.Statistics().AvgRtt
 
+	m.HasChecked, m.Status, m.ResponseTime = true, true, pinger.Statistics().AvgRtt
 }
 
 func (m *Monitor) checkHTTP() {
 	if m.Config.Port == 0 {
 		m.Config.Port = 80
 	}
+	// TODO: Calcul response time
 	resp, err := http.Get(fmt.Sprintf("%s:%d", m.Config.Hostname, m.Config.Port))
 
 	if err != nil {
-		log.Default().Println(err)
 		m.Status, m.StatusCode = false, 0
-
 		return
 	}
 	defer resp.Body.Close()
-	m.Status, m.StatusCode = true, resp.StatusCode
+	m.HasChecked, m.Status, m.StatusCode = true, true, resp.StatusCode
+}
 
+func (m Monitor) check(wg *sync.WaitGroup) {
+	if m.Config.RequestType == "HTTP" {
+		m.checkHTTP()
+	}
+	if m.Config.RequestType == "ICMP" {
+		m.checkICMP()
+	}
+
+	// TODO: Setup alerting here if something is wrong
+	// TODO: Expose HTTP Status page
+	if m.HasChecked {
+		log.Default().Println(m.Config.Name, ": ", m.ResponseTime)
+	} else {
+		log.Default().Println(m.Config.Name, " has not been checked. Please verify the configuration")
+	}
+	wg.Done()
 }
 
 func main() {
-	rawConfigFile, err := os.ReadFile("./config.json") // TODO: Replace config file path using cli args -c /path/to/config/file
+	for {
+		var wg sync.WaitGroup
+		rawConfigFile, err := os.ReadFile("./config.json") // TODO: Replace config file path using cli args -c /path/to/config/file
 
-	if err != nil {
-		panic("Cannot read config file")
-	}
-	var configs []MonitorConfig
-
-	json.Unmarshal(rawConfigFile, &configs)
-	for _, config := range configs {
-		monitor := Monitor{Config: config}
-
-		if monitor.Config.RequestType == "HTTP" {
-			monitor.checkHTTP()
+		if err != nil {
+			panic("Cannot read config file")
 		}
-		if monitor.Config.RequestType == "ICMP" {
-			monitor.checkICMP()
-		}
+		var configs []MonitorConfig
 
-		// TODO: Setup alerting here if something is wrong
-		// TODO: Expose HTTP Status page
-		log.Default().Println(monitor)
+		json.Unmarshal(rawConfigFile, &configs)
+		wg.Add(len(configs))
+
+		for _, config := range configs {
+			monitor := Monitor{Config: config}
+			go monitor.check(&wg)
+		}
+		wg.Wait()
+		time.Sleep(1 * time.Minute)
 	}
 }
